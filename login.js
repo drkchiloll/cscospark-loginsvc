5a36890478ed3a386561a3b7e1d99bf9278e01f5
drkchiloll <samwomack_@me.com>
2016-01-14 16:29:11 -0600

    completed Machine Authorization

--- login.js
+++ login.js
@@ -1,5 +1,7 @@
 var Promise = require('bluebird'),
     request = require('request'),
+    cheerio = require('cheerio'),
+    url = require('url'),
     qs = require('qs'),
     config = require('./config');
 
@@ -44,13 +46,61 @@ var qs = {
   gx_charset: 'UTF-8'
 };
 
+var idsTokForm = (code) => ({
+    security_code : code,
+    response_type: 'code',
+    client_id: config.id,
+    decision: 'accept'
+});
+
+var authQs = () => ({
+    response_type: 'code',
+    client_id: config.id,
+    redirect_uri: config.redirectUri,
+    service: 'webex-squared',
+    scope: scopes
+});
+
+var axxTokForm = (code) => ({
+    grant_type:'authorization_code',
+    client_id: config.id,
+    client_secret: config.secret,
+    code: code,
+    redirect_uri: config.redirectUri
+});
+
 (() => {
   request({
     uri: initLoginUrl,
     qs: qs,
     jar: true
   }, function(err, resp, body) {
-    console.log(resp.statusCode);
-    console.log(body);
+    // console.log(body);
+    var $ = cheerio.load(body);
+    var tmpCode;
+    tmpCode = $('input[type="hidden"]')
+      .attr('name', 'security_code')
+      .val();
+    if(tmpCode.length === 64) {
+      // Continue
+      request.post({
+        url: authUrl,
+        jar: true,
+        qs: authQs(),
+        form: idsAuthForm(tmpCode)
+      }, (err, res, body) => {
+        var code = url.parse(res.headers.location)
+          .query
+          .replace(/\S+=/i, '');
+        request.post({
+          uri: 'https://api.ciscospark.com/v1/access_token',
+          form: axxAuthForm(code),
+        }, (err, res, body) => {
+          console.log(body);
+        })
+      })
+    } else {
+      console.log('something went wrong');
+    }
   });
 }());
